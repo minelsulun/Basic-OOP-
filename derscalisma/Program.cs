02b892e1123bafa0fe9c2d25e1657145bd2bb6e2
minel <minelsulun.3@gmail.com>
2024-09-01 18:04:24 +0300

    Add project files.

---
+++ derscalisma/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace derscalisma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { // Liste oluşturma ve eleman ekleme
+            List<Animal> animalList = new List<Animal>();
+            animalList.Add(new Dog());
+            animalList.Add(new Cat());
+            animalList.Add(new Dog());
+
+            // Liste üzerinde işlem yapma
+            foreach (var animal in animalList)
+            {
+                // Animal türünden bir nesne üzerinden Speak metodunu çağır
+                animal.Speak();
+            }
+            Console.ReadKey();
+        }
+    }
+}
